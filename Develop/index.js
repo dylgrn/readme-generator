eab07ce18ce33ac76d4825d7c7d1309361c1ad0f
dylan green <dylgrn.github.io>
2021-05-23 17:20:52 -0500

    working out bugs

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,128 @@
 // TODO: Include packages needed for this application
-
+const markDown = require('./utils/generateMarkdown');
+const fs = require('fs');
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+            validate: projectNameInput => {
+              if (projectNameInput) {
+                return true;
+              } else {
+                console.log('Please enter your project name!');
+                return false;
+              }
+            }  
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Give a description of your app.',
+            validate: projectDescriptionInput => {
+              if (projectDescriptionInput) {
+                return true;
+              } else {
+                console.log('Project descreption required!');
+                return false;
+              }
+            }  
+        },
+        {
+            type: 'input',
+            name: 'githubusername',
+            message: 'Whst is your github username?',
+            validate: gitUserNameInput => {
+                if (gitUserNameInput) {
+                  return true;
+                } else {
+                  console.log('Github username required!');
+                  return false;
+                }
+              }  
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Is there any installation instructions?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Give a description of the usage of your app',
+            validate: projectusageInput => {
+              if (projectusageInput) {
+                return true;
+              } else {
+                console.log('Usage description required!');
+                return false;
+              }
+            }  
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Provide a list of collaborators if any'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'list tests for your application'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0']
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'List features of your app'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'List contributers for your app'
+        }
+])
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/README.md', fileName, data, err => {
+            if (err) {
+              reject(err);
+              return;
+            }
+            resolve({
+              ok: true,
+              message: 'File created!'
+            });
+          });
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// async function init() {
+//     try {
+//         const answer = await promptQuestions();
+//         writeToFile(answer)
+//         console.log('README successfully created!')
+//     }   catch(err) {
+//         console.log(err)
+//     }
+// }
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+promptQuestions()
+.then(readMeData => {
+    return markDown(readMeData)
+})
+.then(readMeFile => {
+    return writeToFile(readMeFile)
+})
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,86 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(!license) {
+    return '';
+  }
+  return `
+## Badges
+
+![badge](https:img.shields.io/badge/license-${license.license}-brightgreen)
+  `
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(!license) {
+    return '';
+  }
+  return `
+  ## License
+
+  ![badge](https:img.shields.io/badge/license-${license.license}-brightgreen)
+  `
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license) {
+    return '';
+  }
+  return `
+  ## License
+
+  ${renderLicenseLink(license)}
+  `
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+generateMarkdown = input => {
+  return `
+# ${input.title}
 
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+## Description 
+${input.description}
+  
+## Installation
+
+${input.installation}
+  
+## Usage
+
+${input.usuage} 
+  
+## Credits
+
+${input.credits}
+
+${renderLicenseSection(license)}
+
+${renderLicenseBadge(license)}
+  
+## Features
+
+${input.features}
+
+## Contributing
+
+${input.contributing}
+
+## Tests
+
+${input.test}
+
+## https://github.com/${input.githubusername}
 `;
 }
 
